c92fbce368702180faf61a3f1a81925116c6e145
sawan <sawank01@outlook.com>
2022-02-13 21:47:04 +0530

    fixed issues

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -6,14 +6,16 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
-    HashMap<Furniture,Integer> furnitureMap = new HashMap<Furniture,Integer>();
+    HashMap<Furniture,Integer> furnitureMap = new HashMap<>();
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
-        furnitureMap = new HashMap<Furniture,Integer>();
+        furnitureMap.put(Furniture.CHAIR,0);
+        furnitureMap.put(Furniture.COUCH,0);
+        furnitureMap.put(Furniture.TABLE,0);
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
@@ -32,22 +34,22 @@ public class FurnitureOrder implements FurnitureOrderInterface {
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        int quantity = 0; float costChair=0, costCouch=0, costTable=0;
+        int quantity; float costChair=0, costCouch=0, costTable=0;
         if(!furnitureMap.isEmpty()){
             for(Furniture furniture : furnitureMap.keySet()){
                 quantity = furnitureMap.get(furniture);
                 if(Furniture.CHAIR.equals(furniture)){
-                    costChair = quantity*Furniture.COUCH.cost();
+                    costChair = quantity*Furniture.CHAIR.cost();
                 }else if(Furniture.COUCH.equals(furniture)){
                     costCouch = quantity*Furniture.COUCH.cost();
                 }else if(Furniture.TABLE.equals(furniture)){
                     costTable = quantity*Furniture.TABLE.cost();
                 }
             }
-            return costChair+costCouch+costTable;
+
         }
 
-        return -1.0f;
+        return costChair+costCouch+costTable;
     }
 
     public int getTypeCount(Furniture type) {
@@ -55,37 +57,36 @@ public class FurnitureOrder implements FurnitureOrderInterface {
         if(!furnitureMap.isEmpty()){
             return furnitureMap.get(type);
         }
-        return -1;
+        return 0;
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
         if(!furnitureMap.isEmpty()){
-            if(!furnitureMap.isEmpty()){
-                float cost = 0;
-                int quantity = furnitureMap.get(type);
-                if(Furniture.CHAIR.equals(type)){
-                    cost = quantity*Furniture.COUCH.cost();
-                }else if(Furniture.COUCH.equals(type)){
-                    cost = quantity*Furniture.COUCH.cost();
-                }else if(Furniture.TABLE.equals(type)){
-                    cost = quantity*Furniture.TABLE.cost();
-                }
-                return cost;
+            float cost = 0;
+            int quantity = furnitureMap.get(type);
+            if(Furniture.CHAIR.equals(type) && quantity>0){
+                cost = quantity*Furniture.CHAIR.cost();
+            }else if(Furniture.COUCH.equals(type)&& quantity>0){
+                cost = quantity*Furniture.COUCH.cost();
+            }else if(Furniture.TABLE.equals(type)&& quantity>0){
+                cost = quantity*Furniture.TABLE.cost();
             }
+            return cost;
         }
-        return -1.0f;
+        return 0.0f;
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
+        int quantity = 0;
         if(!furnitureMap.isEmpty()){
-            int quantity = 0;
+
             for(Furniture furniture : furnitureMap.keySet()){
                 quantity = quantity+ furnitureMap.get(furniture);
             }
-            return quantity;
+
         }
-        return -1;
+        return quantity;
     }
 }
